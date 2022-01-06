ed7b12dced8a3d5de6aac1b5d29f69f8593558a8
Deependu Jha <deependujha21@gmail.com>
2022-01-06 11:38:11 +0530

    changed allowed host

--- schat_backend/settings.py
+++ schat_backend/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-3sb3yzam=yqq4ernnj8c55^w!(eff$*$ctx0f!sfymz$ha0@r@
 DEBUG = False
 
 ALLOWED_HOSTS = [
-#     '127.0.0.1','schhat.herokuapp.com',
-    '*'
+#     'schat-backend.azurewebsites.net',
 
 ]
 
